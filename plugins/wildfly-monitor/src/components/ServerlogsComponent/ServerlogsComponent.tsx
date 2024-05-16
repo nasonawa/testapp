6b76598b8b4aa63e553894996559b9840b63d118
nasonawa <nasonawa@redhat.com>
2024-05-16 21:33:52 +0530

    server log component complete

--- plugins/wildfly-monitor/src/components/ServerlogsComponent/ServerlogsComponent.tsx
+++ plugins/wildfly-monitor/src/components/ServerlogsComponent/ServerlogsComponent.tsx
@@ -3,26 +3,26 @@ import React, { useEffect, useState } from 'react';
 import {useApi,configApiRef} from '@backstage/core-plugin-api'
 import DigestClient from 'digest-fetch';
 import {LogViewer} from '@backstage/core-components';
-import axios from 'axios';
 
 export const ServerlogsComponent = () => {
 
   const  [logdata,setData] = useState('');
   const config = useApi(configApiRef);
 
-  const username = 'user';
-  const password = 'pass';
+  const username = config.get('wildfly.username');
+  const password = config.get('wildfly.password');
   
-  const BASE_URL = 'http://localhost:9990/management/subsystem/logging/log-file/server.log?operation=attribute&name=stream&useStreamAsResponse';
-
+  const BASE_URL = 'http://localhost:9990/management';
   const client = new DigestClient('user', 'pass') 
-  const fetchdata =   () => {
-
-    client.fetch(`${BASE_URL}/subsystem/logging/log-file/server.log?operation=attribute&name=stream&useStreamAsResponse`, {})
-    .then(resp=>resp.text())
-    .then(data=>setData(data))
-    .catch(e=>console.error(e))
-
+  
+  const fetchdata =  async () => {
+    try {
+      const response = await client.fetch(`${BASE_URL}/subsystem/logging/log-file/server.log?operation=attribute&name=stream&useStreamAsResponse`, {})
+      const data = await response.text();
+      setData(data);
+    }catch(e) {
+      console.log(e);
+    }
   };
 
   useEffect(()=> {
